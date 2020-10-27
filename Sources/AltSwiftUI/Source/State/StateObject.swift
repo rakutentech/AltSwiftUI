427d0c2c891c814fb65d748123757f1f660beaf1
Kevin Wong <kevin.wl.02@gmail.com>
2020-10-27 12:16:24 +0900

    Separate StateObject from ObservedObject to fix dynamic member lookup (#37)
    Co-authored-by: Kevin Wong <kevin.a.wong@rakuten.com>

--- Sources/AltSwiftUI/Source/State/StateObject.swift
+++ Sources/AltSwiftUI/Source/State/StateObject.swift
@@ -17,14 +17,46 @@ import Foundation
 /// - ObservedObject owned by view: Gets recreated (re-initialized)
 /// - StateObject owned by view: Keeps same instance
 @propertyWrapper
-public class StateObject<ObjectType: ObservableObject>: ObservedObject<ObjectType> {
-    override public var wrappedValue: ObjectType {
+public class StateObject<ObjectType: ObservableObject> {
+    /// A wrapper of the underlying `ObservableObject` that can create
+    /// `Binding`s to its properties using dynamic member lookup.
+    @dynamicMemberLookup public struct Wrapper {
+
+        var parent: StateObject
+        
+        /// Creates a `Binding` to a value semantic property of a
+        /// reference type.
+        ///
+        /// If `Value` is not value semantic, the updating behavior for
+        /// any views that make use of the resulting `Binding` is
+        /// unspecified.
+        public subscript<Subject>(dynamicMember keyPath: ReferenceWritableKeyPath<ObjectType, Subject>) -> Binding<Subject> {
+            Binding(get: { self.parent.wrappedValue[keyPath: keyPath] },
+                    set: { self.parent.wrappedValue[keyPath: keyPath] = $0 })
+        }
+    }
+    
+    var _wrappedValue: ObjectType
+    
+    public init(wrappedValue value: ObjectType) {
+        _wrappedValue = value
+        value.setupPublishedValues()
+    }
+    
+    /// The internal value of this wrapper type.
+    public var wrappedValue: ObjectType {
         get {
-            super.wrappedValue
+            EnvironmentHolder.currentBodyViewBinderStack.last?.registerStateNotification(origin: _wrappedValue)
+            return _wrappedValue
         }
         set {
-            super.wrappedValue = newValue
+            _wrappedValue = newValue
         }
+    }
+    
+    /// The direct value of this wrapper, as accessing $foo on a @EnvironmentObject property.
+    public var projectedValue: StateObject<ObjectType>.Wrapper {
+        Wrapper(parent: self)
     }
 }
 
